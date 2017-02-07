58f063f08cfd7e020183224d4ff533a1f2819a47
negative-seven <tehseven1@gmail.com>
2017-02-07 16:30:40 +0100

    Added iteration through all sprites

--- nd_level_extract/main.cpp
+++ nd_level_extract/main.cpp
@@ -19,16 +19,28 @@ int main()
 
 	unsigned int address_base = getBaseAddress(handle_process, "NecroDancer.exe");
 
-	int sprite_x = address_base;
+	int sprite = address_base;
 
-	std::vector<int> pointers = { 0x3FC2BC, 0x10, 0x10, 0x10, 0x18, 0x14 };
+	std::vector<int> pointers = { 0x3FC2BC, 0x10, 0x10, 0x10 }; // 2nd pointer to 0x48 = traps, 3rd to 0x38 = miniboss
 
 	for (unsigned int i = 0; i < pointers.size(); i++)
 	{
-		sprite_x = readMemoryInt(handle_process, sprite_x + pointers[i]);
+		sprite = readMemoryInt(handle_process, sprite + pointers[i]);
 	}
 
-	std::cout << sprite_x;
+	while (true)
+	{
+		try
+		{
+			std::cout << readMemoryInt(handle_process, readMemoryInt(handle_process, sprite + 0x18) + 0x14) << "\n";
+		}
+		catch (std::runtime_error)
+		{
+			break;
+		}
+
+		sprite = readMemoryInt(handle_process, sprite + 0x10);
+	}
 
 	closeProcess(handle_process);
 
