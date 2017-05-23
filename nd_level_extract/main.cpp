64b60a278daa6c786c3f73b95280e1681252a8ef
negative-seven <tehseven1@gmail.com>
2017-05-23 18:18:09 +0200

    Use different lists in memory for different objects

--- nd_level_extract/main.cpp
+++ nd_level_extract/main.cpp
@@ -11,24 +11,21 @@
 #define ATTR_NOITEM (1 << 19) // Set to "no_item"
 #define ATTR_BOOL (1 << 20) // Set to 1 if greater than 1
 
+class obj
+{
+public:
+	unsigned int pointer;
+	std::map<std::string, int> attributes;
+};
+
 class objType
 {
 public:
 	std::string name_singular;
 	std::string name_plural;
 	std::map<std::string, unsigned int> attributes;
-	std::vector<std::map<std::string, int>> objList;
-	std::vector<unsigned int> ids; // Compare with [[obj+0x0]+0x0]
-
-	objType() {}
-
-	objType(std::string name_singular, std::string name_plural, std::map<std::string, unsigned int> attributes, std::vector<unsigned int> ids)
-	{
-		this->name_singular = name_singular;
-		this->name_plural = name_plural;
-		this->attributes = attributes;
-		this->ids = ids;
-	}
+	std::vector<obj> objList;
+	std::vector<unsigned int> offsets_firstObj;
 };
 
 int main()
@@ -49,10 +46,14 @@ int main()
 
 	unsigned int address_base = getBaseAddress(handle_process, "NecroDancer.exe");
 
-	std::vector<objType> objType_list;
-	std::map<std::string, unsigned int> attributes; // Temporary
+	// Create all instances of objType
 
-	attributes = {
+	std::vector<objType> objType_list;
+
+	objType objType_tiles;
+	objType_tiles.name_singular = "tile";
+	objType_tiles.name_plural = "tiles";
+	objType_tiles.attributes = {
 		{"cracked", ATTR_READBYTE | 0x60},
 		{"torch", ATTR_BOOL | 0x64},
 		{"type", 0x58},
@@ -60,29 +61,38 @@ int main()
 		{"y", 0x18},
 		{"zone", 0x5C} // TODO: detect!
 	};
-	objType objType_tiles = objType("tile", "tiles", attributes, {0x1B3890});
-	objType_list.push_back(objType_tiles);
+	objType_tiles.offsets_firstObj = {0x4026CC, 0x10, 0x10, 0x10};
+	// Don't add to objType_list yet, special case
 
-	attributes = {
+	objType objType_traps;
+	objType_traps.name_singular = "trap";
+	objType_traps.name_plural = "traps";
+	objType_traps.attributes = {
 		{"subtype", ATTR_READBYTE | 0x10C},
 		{"type", 0xF0},
 		{"x", 0x14},
 		{"y", 0x18}
 	};
-	objType objType_traps = objType("trap", "traps", attributes, {0x1C1E00});
+	objType_traps.offsets_firstObj = {0x40246C, 0x10, 0x10};
 	objType_list.push_back(objType_traps);
 
-	attributes = {
+	objType objType_enemies;
+	objType_enemies.name_singular = "enemy";
+	objType_enemies.name_plural = "enemies";
+	objType_enemies.attributes = {
 		{"beatDelay", ATTR_HARDCODED | 0}, // 0 is correct for most enemies
 		{"lord", ATTR_HARDCODED | 0}, // TODO: detect
 		{"type", 0x10C},
 		{"x", 0x14},
 		{"y", 0x18}
 	};
-	objType objType_enemies = objType("enemy", "enemies", attributes, {0x1E39A0, 0x1AD5B0, 0x218D30, 0xF3AC0});
+	objType_enemies.offsets_firstObj = {0x4024D0, 0x10, 0x10};
 	objType_list.push_back(objType_enemies);
 
-	attributes = {
+	objType objType_items;
+	objType_items.name_singular = "item";
+	objType_items.name_plural = "items";
+	objType_items.attributes = {
 		{"bloodCost", ATTR_HARDCODED | 0}, // Float at offset 0x13C
 		{"saleCost", ATTR_HARDCODED | 0}, // Float at offset 0x140
 		{"singleChoice", ATTR_HARDCODED | 0}, // TODO: detect
@@ -90,10 +100,13 @@ int main()
 		{"x", 0x14},
 		{"y", 0x18}
 	};
-	objType objType_items = objType("item", "items", attributes, {0x180C40, 0x170890});
+	objType_items.offsets_firstObj = {0x402468, 0x10, 0x10};
 	objType_list.push_back(objType_items);
 
-	attributes = {
+	objType objType_chests;
+	objType_chests.name_singular = "chest";
+	objType_chests.name_plural = "chests";
+	objType_chests.attributes = {
 		{"color", 0xF0},
 		{"contents", ATTR_NOITEM}, // TODO: detect
 		{"hidden", ATTR_HARDCODED | 0}, // TODO: detect
@@ -102,104 +115,133 @@ int main()
 		{"x", 0x14},
 		{"y", 0x18}
 	};
-	objType objType_chests = objType("chest", "chests", attributes, {0x1C6C80, 0x1833F0});
+	objType_chests.offsets_firstObj = {0x4021B4, 0x10, 0x10};
 	objType_list.push_back(objType_chests);
 
-	attributes = {
+	objType objType_crates;
+	objType_crates.name_singular = "crate";
+	objType_crates.name_plural = "crates";
+	objType_crates.attributes = {
 		{"contents", ATTR_NOITEM}, // TODO: detect (unicode string 0x230)
 		{"type", 0x22C},
 		{"x", 0x14},
 		{"y", 0x18}
 	};
-	objType objType_crates = objType("crate", "crates", attributes, {0x1A7F00});
+	objType_crates.offsets_firstObj = {0x402198, 0x10, 0x10};
 	objType_list.push_back(objType_crates);
 
-	attributes = {
+	objType objType_shrines;
+	objType_shrines.name_singular = "shrine";
+	objType_shrines.name_plural = "shrines";
+	objType_shrines.attributes = {
 		{"type", 0xF0},
 		{"x", 0x14},
 		{"y", 0x18}
 	};
-	objType objType_shrines = objType("shrine", "shrines", attributes, {0x167220});
+	objType_shrines.offsets_firstObj = {0x4021E8, 0x10, 0x10};
 	objType_list.push_back(objType_shrines);
 
-	// Get all objects
+	// Get all objects of each type (except tiles)
 
-	std::vector<unsigned int> obj_pointer_offsets = {0x3FF6CC, 0x10, 0x10, 0x10};
-	int obj = address_base;
-
-	for (unsigned int i = 0; i < obj_pointer_offsets.size(); i++)
+	for (unsigned int i = 0; i < objType_list.size(); i++)
 	{
-		obj = readMemoryInt(handle_process, obj + obj_pointer_offsets[i]);
-	}
+		objType objType = objType_list[i];
 
-	while (true)
-	{
-		int temp = readMemoryInt(handle_process, obj + 0x18);
+		int temp = address_base;
 
-		if (!temp)
+		for each (unsigned int offset in objType.offsets_firstObj)
 		{
-			break;
+			temp = readMemoryInt(handle_process, temp + offset);
 		}
 
-		int obj_id = readMemoryInt(handle_process, readMemoryInt(handle_process, temp + 0x0) + 0x0) - address_base;
-
-		bool found = false;
-		unsigned int i = 0;
-
-		while(i < objType_list.size())
+		while (true)
 		{
-			for (unsigned int j = 0; j < objType_list[i].ids.size(); j++)
-			{
-				if (obj_id == objType_list[i].ids[j])
-				{
-					found = true;
-					break;
-				}
-			}
+			int temp2 = readMemoryInt(handle_process, temp + 0x18);
 
-			if (found)
+			if (!temp2)
 			{
 				break;
 			}
 
-			i++;
+			obj obj;
+			obj.pointer = temp2;
+			objType_list[i].objList.push_back(obj);
+
+			temp = readMemoryInt(handle_process, temp + 0x10);
+		}
+	}
+
+	// Get all tile objects
+
+	int temp = address_base;
+
+	for each (unsigned int offset in objType_tiles.offsets_firstObj)
+	{
+		temp = readMemoryInt(handle_process, temp + offset);
+	}
+
+	while (true)
+	{
+		int temp2 = readMemoryInt(handle_process, temp + 0x18);
+		temp = readMemoryInt(handle_process, temp + 0x10);
+
+		if (!temp2)
+		{
+			break;
 		}
 
-		if (!found)
+		for each (objType objType in objType_list)
 		{
-			std::cout << "Error: object ID 0x" << std::hex << obj_id << std::dec << " is undefined. Detected object coordinates: " << readMemoryInt(handle_process, temp + 0x14) << ", " << readMemoryInt(handle_process, temp + 0x18) << "\n";
+			for each (obj obj in objType.objList)
+			{
+				if (obj.pointer == temp2)
+				{
+					continue;
+				}
+			}
 		}
-		else
+
+		obj obj;
+		obj.pointer = temp2;
+		objType_tiles.objList.push_back(obj);
+	}
+
+	objType_list.insert(objType_list.begin(), objType_tiles);
+
+	// Get attributes of each object
+
+	for (unsigned int i = 0; i < objType_list.size(); i++)
+	{
+		for (unsigned int j = 0; j < objType_list[i].objList.size(); j++)
 		{
-			std::map<std::string, int> obj_attributes;
+			obj obj = objType_list[i].objList[j];
 
 			for (auto const& p : objType_list[i].attributes)
 			{
 				if (p.second & ATTR_HARDCODED)
 				{
-					obj_attributes.insert(std::make_pair(p.first, LOWORD(p.second)));
+					obj.attributes.insert(std::make_pair(p.first, LOWORD(p.second)));
 				}
 				else if (p.second & ATTR_READBYTE)
 				{
-					obj_attributes.insert(std::make_pair(p.first, (byte)readMemoryInt(handle_process, temp + LOWORD(p.second), 1)));
+					obj.attributes.insert(std::make_pair(p.first, (byte)readMemoryInt(handle_process, obj.pointer + LOWORD(p.second), 1)));
 				}
 				else if (p.second & ATTR_NOITEM)
 				{
-					obj_attributes.insert(std::make_pair(p.first, p.second));
+					obj.attributes.insert(std::make_pair(p.first, p.second));
 				}
 				else if (p.second & ATTR_BOOL)
 				{
-					obj_attributes.insert(std::make_pair(p.first, readMemoryInt(handle_process, temp + LOWORD(p.second)) > 1));
+					obj.attributes.insert(std::make_pair(p.first, readMemoryInt(handle_process, obj.pointer + LOWORD(p.second)) > 1));
 				}
 				else
 				{
-					obj_attributes.insert(std::make_pair(p.first, readMemoryInt(handle_process, temp + LOWORD(p.second))));
+					obj.attributes.insert(std::make_pair(p.first, readMemoryInt(handle_process, obj.pointer + LOWORD(p.second))));
 				}
 			}
 
-			objType_list[i].objList.push_back(obj_attributes);
+			objType_list[i].objList[j] = obj;
 		}
-		obj = readMemoryInt(handle_process, obj + 0x10);
 	}
 
 	// Close handle for the game
@@ -237,7 +279,7 @@ int main()
 		{
 			pugi::xml_node node_level_object = node_level_objects.append_child(objType.name_singular.c_str());
 
-			for (auto const& p : objType.objList[j])
+			for (auto const& p : objType.objList[j].attributes)
 			{
 				/*if (p.second & ATTR_READUSTR)
 				{
