5bc01c505637a069dd5cc4f169f0d7c8e1584d45
negative-seven <tehseven1@gmail.com>
2017-07-22 22:06:04 +0200

    objType_list now contains objType pointers

--- nd_level_extract/main.cpp
+++ nd_level_extract/main.cpp
@@ -49,12 +49,12 @@ int main()
 
 	// Create all instances of objType
 
-	std::vector<objType> objType_list;
+	std::vector<objType *> objType_list;
 
-	objType objType_tiles;
-	objType_tiles.name_singular = "tile";
-	objType_tiles.name_plural = "tiles";
-	objType_tiles.attributes = {
+	objType *objType_tiles = new objType();
+	objType_tiles->name_singular = "tile";
+	objType_tiles->name_plural = "tiles";
+	objType_tiles->attributes = {
 		{"cracked", ATTR_READBYTE | 0x60},
 		{"torch", ATTR_BOOL | 0x64},
 		{"type", 0x58},
@@ -62,38 +62,38 @@ int main()
 		{"y", 0x18},
 		{"zone", 0x5C}
 	};
-	objType_tiles.offsets_firstObj = {0x42DBEC, 0x10, 0x10, 0x10};
+	objType_tiles->offsets_firstObj = {0x42DBEC, 0x10, 0x10, 0x10};
 	// Don't add to objType_list yet, special case
 
-	objType objType_traps;
-	objType_traps.name_singular = "trap";
-	objType_traps.name_plural = "traps";
-	objType_traps.attributes = {
+	objType *objType_traps = new objType();
+	objType_traps->name_singular = "trap";
+	objType_traps->name_plural = "traps";
+	objType_traps->attributes = {
 		{"subtype", ATTR_HARDCODED | 0}, // Special case
 		{"type", 0xF4},
 		{"x", 0x14},
 		{"y", 0x18}
 	};
-	objType_traps.offsets_firstObj = {0x42D97C, 0x10, 0x10};
+	objType_traps->offsets_firstObj = {0x42D97C, 0x10, 0x10};
 	objType_list.push_back(objType_traps);
 
-	objType objType_enemies;
-	objType_enemies.name_singular = "enemy";
-	objType_enemies.name_plural = "enemies";
-	objType_enemies.attributes = {
+	objType *objType_enemies = new objType();
+	objType_enemies->name_singular = "enemy";
+	objType_enemies->name_plural = "enemies";
+	objType_enemies->attributes = {
 		{"beatDelay", 0x114},
 		{"lord", ATTR_READBYTE | 0x118},
 		{"type", 0x110},
 		{"x", 0x14},
 		{"y", 0x18}
 	};
-	objType_enemies.offsets_firstObj = {0x42D9E0, 0x10, 0x10};
+	objType_enemies->offsets_firstObj = {0x42D9E0, 0x10, 0x10};
 	objType_list.push_back(objType_enemies);
 
-	objType objType_items;
-	objType_items.name_singular = "item";
-	objType_items.name_plural = "items";
-	objType_items.attributes = {
+	objType *objType_items = new objType();
+	objType_items->name_singular = "item";
+	objType_items->name_plural = "items";
+	objType_items->attributes = {
 		{"bloodCost", ATTR_READFLOAT | 0x148},
 		{"saleCost", 0x100},
 		{"singleChoice", ATTR_READBYTE | 0xF8},
@@ -101,13 +101,13 @@ int main()
 		{"x", 0x14},
 		{"y", 0x18}
 	};
-	objType_items.offsets_firstObj = {0x42D978, 0x10, 0x10};
+	objType_items->offsets_firstObj = {0x42D978, 0x10, 0x10};
 	objType_list.push_back(objType_items);
 
-	objType objType_chests;
-	objType_chests.name_singular = "chest";
-	objType_chests.name_plural = "chests";
-	objType_chests.attributes = {
+	objType *objType_chests = new objType();
+	objType_chests->name_singular = "chest";
+	objType_chests->name_plural = "chests";
+	objType_chests->attributes = {
 		{"color", 0xF4},
 		{"contents", ATTR_READUSTR | 0xF8},
 		{"hidden", 0x9C},
@@ -116,41 +116,41 @@ int main()
 		{"x", 0x14},
 		{"y", 0x18}
 	};
-	objType_chests.offsets_firstObj = {0x42D938, 0x10, 0x10};
+	objType_chests->offsets_firstObj = {0x42D938, 0x10, 0x10};
 	objType_list.push_back(objType_chests);
 
-	objType objType_crates;
-	objType_crates.name_singular = "crate";
-	objType_crates.name_plural = "crates";
-	objType_crates.attributes = {
+	objType *objType_crates = new objType();
+	objType_crates->name_singular = "crate";
+	objType_crates->name_plural = "crates";
+	objType_crates->attributes = {
 		{"contents", ATTR_READUSTR | 0x238},
 		{"type", 0x234},
 		{"x", 0x14},
 		{"y", 0x18}
 	};
-	objType_crates.offsets_firstObj = {0x42D6AC, 0x10, 0x10};
+	objType_crates->offsets_firstObj = {0x42D6AC, 0x10, 0x10};
 	objType_list.push_back(objType_crates);
 
-	objType objType_shrines;
-	objType_shrines.name_singular = "shrine";
-	objType_shrines.name_plural = "shrines";
-	objType_shrines.attributes = {
+	objType *objType_shrines = new objType();
+	objType_shrines->name_singular = "shrine";
+	objType_shrines->name_plural = "shrines";
+	objType_shrines->attributes = {
 		{"type", 0xF4},
 		{"x", 0x14},
 		{"y", 0x18}
 	};
-	objType_shrines.offsets_firstObj = {0x42D6F8, 0x10, 0x10};
+	objType_shrines->offsets_firstObj = {0x42D6F8, 0x10, 0x10};
 	objType_list.push_back(objType_shrines);
 
 	// Get all objects of each type (except tiles)
 
 	for (unsigned int i = 0; i < objType_list.size(); i++)
 	{
-		objType objType = objType_list[i];
+		objType *objType = objType_list[i];
 
 		int temp = address_base;
 
-		for each (unsigned int offset in objType.offsets_firstObj)
+		for each (unsigned int offset in objType->offsets_firstObj)
 		{
 			temp = readMemoryInt(handle_process, temp + offset);
 		}
@@ -166,7 +166,7 @@ int main()
 
 			obj obj;
 			obj.pointer = temp2;
-			objType_list[i].objList.push_back(obj);
+			objType_list[i]->objList.push_back(obj);
 
 			temp = readMemoryInt(handle_process, temp + 0x10);
 		}
@@ -176,7 +176,7 @@ int main()
 
 	int temp = address_base;
 
-	for each (unsigned int offset in objType_tiles.offsets_firstObj)
+	for each (unsigned int offset in objType_tiles->offsets_firstObj)
 	{
 		temp = readMemoryInt(handle_process, temp + offset);
 	}
@@ -193,9 +193,9 @@ int main()
 
 		bool isTile = true;
 
-		for each (objType objType in objType_list)
+		for each (objType *objType in objType_list)
 		{
-			for each (obj obj in objType.objList)
+			for each (obj obj in objType->objList)
 			{
 				if (obj.pointer == temp2)
 				{
@@ -214,7 +214,7 @@ int main()
 		{
 			obj obj;
 			obj.pointer = temp2;
-			objType_tiles.objList.push_back(obj);
+			objType_tiles->objList.push_back(obj);
 		}
 	}
 
@@ -224,11 +224,11 @@ int main()
 
 	for (unsigned int i = 0; i < objType_list.size(); i++)
 	{
-		for (unsigned int j = 0; j < objType_list[i].objList.size(); j++)
+		for (unsigned int j = 0; j < objType_list[i]->objList.size(); j++)
 		{
-			obj obj = objType_list[i].objList[j];
+			obj obj = objType_list[i]->objList[j];
 
-			for (auto const& p : objType_list[i].attributes)
+			for (auto const& p : objType_list[i]->attributes)
 			{
 				std::string value;
 
@@ -285,20 +285,20 @@ int main()
 				obj.attributes.insert(std::make_pair(p.first, value));
 			}
 
-			objType_list[i].objList[j] = obj;
+			objType_list[i]->objList[j] = obj;
 		}
 	}
 
 	// Get subtype of all traps (special case)
 
-	for (unsigned int i = 0; i < objType_list[1].objList.size(); i++)
+	for (unsigned int i = 0; i < objType_traps->objList.size(); i++)
 	{
-		obj obj = objType_list[1].objList[i];
+		obj obj = objType_traps->objList[i];
 
 		int offset = obj.attributes["type"] == "1" ? 0x110 : 0x10C;
 		obj.attributes["subtype"] = std::to_string(readMemoryInt(handle_process, obj.pointer + offset));
 		
-		objType_list[1].objList[i] = obj;
+		objType_traps->objList[i] = obj;
 	}
 
 	// Close handle for the game
@@ -309,7 +309,7 @@ int main()
 
 	for (unsigned int i = 0; i < objType_list.size(); i++)
 	{
-		std::cout << "Found " << objType_list[i].objList.size() << " " << objType_list[i].name_plural.c_str() << "\n";
+		std::cout << "Found " << objType_list[i]->objList.size() << " " << objType_list[i]->name_plural.c_str() << "\n";
 	}
 
 	// Generate XML document
@@ -328,15 +328,15 @@ int main()
 
 	for (unsigned int i = 0; i < objType_list.size(); i++)
 	{
-		objType objType = objType_list[i];
+		objType *objType = objType_list[i];
 
-		pugi::xml_node node_level_objects = node_level.append_child(objType.name_plural.c_str());
+		pugi::xml_node node_level_objects = node_level.append_child(objType->name_plural.c_str());
 
-		for (unsigned int j = 0; j < objType.objList.size(); j++)
+		for (unsigned int j = 0; j < objType->objList.size(); j++)
 		{
-			pugi::xml_node node_level_object = node_level_objects.append_child(objType.name_singular.c_str());
+			pugi::xml_node node_level_object = node_level_objects.append_child(objType->name_singular.c_str());
 
-			for (auto const& p : objType.objList[j].attributes)
+			for (auto const& p : objType->objList[j].attributes)
 			{
 				node_level_object.append_attribute(p.first.c_str()) = p.second.c_str();
 			}
